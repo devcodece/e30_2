74845d942d1f2d6240ed9b515f727d0e56eaba4c
devcodece <devcodece@gmail.com>
2020-09-27 22:11:21 -0600

    Primer commit de la parctica ecommerce

---
+++ e30/urls.py
@@ -0,0 +1,31 @@
+"""e30 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from store.views import store, cart, checkout, updateItem
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', store, name = 'store'),
+    path('cart/', cart, name = 'cart'),
+    path('checkout', checkout, name = 'checkout'),
+    path('update_item', updateItem, name = 'update_item')
+]
+
+#Para tener acceso a las imagenes via url
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ static/js/cart.js
@@ -0,0 +1,76 @@
+//Capturar el boton en la variable updateBtns
+var updateBtns = document.getElementsByClassName('update-cart')
+
+//CICLO para acumular cada click que se de sobre cada boton de Add to Cart
+for(i=0; i < updateBtns.length; i++){
+    //Cada vez que se le click al boton
+    updateBtns[i].addEventListener('click', function(){
+        //guardar id
+        var productId = this.dataset.product
+        //guardar accion
+        var action = this.dataset.action
+
+        console.log('productId:', productId, 'Action:', action)
+        console.log('User:', user)
+
+        if(user === 'AnonymousUser'){
+            console.log('Not looged in')
+            //addCookieItem(productId, action)
+        }else{
+            //console.log('User is logged sending data...')
+            updateUserOrder(productId, action)
+        }
+    })
+}
+
+/* function addCookieItem(productId, action){
+    console.log('Not logged in...')
+
+    if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }else{
+            cart[productId]['quantity'] += 1
+        }
+    }
+
+    if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+
+        if(cart[productId]['quantity'] <= 0){
+            console.log('Remove item')
+            delete cart[productId]
+        }
+    }
+    console.log('Cart:', cart)
+    document.cookie = 'cart=' + JSON.stringify(cart) + "; domain = ; path = /"
+    location.reload()
+} */
+
+function updateUserOrder(productId, action){
+    console.log('User is logged in, sending data...')
+
+    //Mandar los datos al backend
+    var url = '/update_item'
+
+    // a traves de fetch
+    fetch(url, {
+        method: 'POST',
+        headers:{
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,
+        },
+        //Datos que se enviaran al backend
+        body:JSON.stringify({'productId': productId, 'action': action})
+    })
+
+
+    .then((response) => {
+        return response.json()
+    })
+
+    .then((data) =>{
+        console.log('data:', data)
+        location.reload()
+    })
+}
